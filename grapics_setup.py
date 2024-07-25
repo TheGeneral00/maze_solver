92ad56edfabeaab98babccedbe34c8034064e598
TheGeneral00 <richard.beldner@outlook.de>
2024-07-25 18:05:58 +0200

    updated graphics setup with point and line classes with according drawing functions

--- grapics_setup.py
+++ grapics_setup.py
@@ -31,3 +31,22 @@ class window:
 
     def close(self):
         self.runnig = False
+
+    def draw_line(self, line, fill_color):
+        line.draw(fill_color)
+
+
+class point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+class line:
+    def __init__(self, point1, point2):
+        self.point1 = point1
+        self.point2 = point2
+
+    def draw(self, canvas, fill_color):
+        canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
+
+
