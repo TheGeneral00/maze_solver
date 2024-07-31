af37f25705022d2b0190aaebc700af8119b78bc7
TheGeneral00 <richard.beldner@outlook.de>
2024-07-31 21:07:01 +0200

    added cell class and drawing function

--- grapics_setup.py
+++ grapics_setup.py
@@ -2,46 +2,46 @@ from tkinter import Tk, BOTH, Canvas
 import tkinter as tk
 
 
-class window:
-    def __init__(self, width, heigth):
+class Window:
+    def __init__(self, width, height):
         self.width = width
-        self.heigth = heigth
+        self.heigth = height
         self.__root = tk.Tk()
-        
+        self.running = False
+
         #setting title for the __root widget
         self.__root.title('Maze Solver')
         
         #creating canvas for drawing graphics
-        self.canvas = Canvas(self.__root, { 'bg': 'black',  })
+        self.canvas = Canvas(self.__root, { 'bg': 'black'})
         self.canvas.pack()
         
-        #creating value to track the state of the window
-        self.running = False
-        
         self.__root.protocol('WM_DELETE_WINDOW', self.close)
+        
+        
 
     def redraw(self):
         self.__root.update_idletasks()
         self.__root.update()
 
     def wait_for_close(self):
-        self.runnig = True
+        self.running = True
         while self.running == True:
             self.redraw()
 
     def close(self):
-        self.runnig = False
+        self.running = False
 
-    def draw_line(self, line, fill_color):
-        line.draw(fill_color)
+    def draw_line(self, line, fill_color='white'):
+        line.draw(self.canvas, fill_color)
 
 
-class point:
+class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
-class line:
+class Line:
     def __init__(self, point1, point2):
         self.point1 = point1
         self.point2 = point2
@@ -49,4 +49,36 @@ class line:
     def draw(self, canvas, fill_color):
         canvas.create_line(self.point1.x, self.point1.y, self.point2.x, self.point2.y, fill=fill_color, width=2)
 
+class Cell:
+    def __init__(self, win):
+        self.has_left_wall = True
+        self.has_right_wall = True
+        self.has_top_wall = True
+        self.has_bottom_wall = True
+        self._x1 = None
+        self._y1 = None
+        self._x2 = None
+        self._y2 = None
+        self._win = win
+
+    def draw(self, x1, y1, x2, y2):
+        #initializing coordinates of the Cell
+        self._x1 = x1
+        self._y1 = y1
+        self._x2 = x2
+        self._y2 = y2 
+
+        #drawing the cell walls
+        if self.has_left_wall == True:
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line)
+        if self.has_right_wall == True:
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line) 
+        if self.has_top_wall == True:
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line)
+        if self.has_bottom_wall == True:
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line)
 
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
-from grapics_setup import window
+from grapics_setup import *
 
 def main():
-    win = window(800, 600)
+    win = Window(800, 600)
+    c = Cell(win)
+    c.has_left_wall = False
+    c.draw(50, 50, 100, 100)
+
+    c = Cell(win)
+    c.has_right_wall = False
+    c.draw(125, 125, 200, 200)
+
+    c = Cell(win)
+    c.has_bottom_wall = False
+    c.draw(225, 225, 250, 250)
+
+    c = Cell(win)
+    c.has_top_wall = False
+    c.draw(300, 300, 500, 500)
     win.wait_for_close()
 
 if __name__ == '__main__':
